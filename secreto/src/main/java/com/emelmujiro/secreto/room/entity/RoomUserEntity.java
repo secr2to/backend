7d0abbd2d7bc7e1dda3307073b66d31900bef700
sanghak lee <yee950419@naver.com>
2025-04-29 00:34:45 +0900

    Fix: entity 오류 수정
    - RoomUserEntity 코드 수정
    
    Related to

--- secreto/src/main/java/com/emelmujiro/secreto/room/entity/RoomUserEntity.java
+++ secreto/src/main/java/com/emelmujiro/secreto/room/entity/RoomUserEntity.java
@@ -1,5 +1,19 @@
 package com.emelmujiro.secreto.room.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "room_user")
 public class RoomUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_user_id")
+    private Long id;
 }
 
