7f79a86117ab29f733daf70f83ee1089d88226c4
h-spear <80024307+h-spear@users.noreply.github.com>
2025-05-03 00:08:57 +0900

    Feat: UserRepository - findByEmail 메서드 추가

--- secreto/src/main/java/com/emelmujiro/secreto/user/repository/UserRepository.java
+++ secreto/src/main/java/com/emelmujiro/secreto/user/repository/UserRepository.java
@@ -9,4 +9,6 @@ import com.emelmujiro.secreto.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByUsername(String username);
+
+	Optional<User> findByEmail(String email);
 }
