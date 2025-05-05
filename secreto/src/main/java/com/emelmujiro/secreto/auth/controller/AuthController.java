a0dbdde2fbc3fa08f4c7d8dc8e6e000fd58c6d1e
h-spear <80024307+h-spear@users.noreply.github.com>
2025-05-05 13:29:37 +0900

    Comment: 개발 필요한 내용 주석 작성 (#12)

--- secreto/src/main/java/com/emelmujiro/secreto/auth/controller/AuthController.java
+++ secreto/src/main/java/com/emelmujiro/secreto/auth/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,13 @@ public class AuthController {
 		return ApiResponse.builder()
 			.data(principal.toLoginResponse())
 			.success();
+	}
+
+	/***
+	 * 액세스 토큰 재발급 API 개발 필요
+	 */
+	@PostMapping("/refresh-access-token")
+	public ResponseEntity<?> refreshAccessToken() {
+		return null;
 	}
 }
