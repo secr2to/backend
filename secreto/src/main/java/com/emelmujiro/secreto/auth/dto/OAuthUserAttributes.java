255844f15527fdb57147d4b82ca113957384ad3c
h-spear <80024307+h-spear@users.noreply.github.com>
2025-05-05 20:43:51 +0900

    Feat: 카카오 OAuth 로그인 연동 (#12)

--- secreto/src/main/java/com/emelmujiro/secreto/auth/dto/OAuthUserAttributes.java
+++ secreto/src/main/java/com/emelmujiro/secreto/auth/dto/OAuthUserAttributes.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import com.emelmujiro.secreto.user.entity.User;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class OAuthUserAttributes {
 
 	private String provider;
@@ -34,8 +36,14 @@ public class OAuthUserAttributes {
 		this.profileUrl = getString(userInfo, "profile_image");
 	}
 
+	@SuppressWarnings("unchecked")
 	private void mapUserAttributeOfKakao(Map<String, Object> attributes) {
 		Map<String, Object> userInfo = getUserInfo(provider, attributes);
+		Map<String, Object> profileInfo = (Map<String, Object>) userInfo.get("profile");
+		this.email = getString(userInfo, "email");
+		this.username = String.valueOf(getLong(attributes, "id"));
+		this.nickname = getString(profileInfo, "nickname");
+		this.profileUrl = getString(profileInfo, "profile_image_url");
 	}
 
 	private void mapUserAttributeOfGoogle(Map<String, Object> attributes) {
@@ -56,7 +64,7 @@ public class OAuthUserAttributes {
 	private static Map<String, Object> getUserInfo(String provider, Map<String, Object> attributes) {
 		return switch (provider) {
 			case "naver" -> (Map<String, Object>) attributes.get("response");
-			case "kakao" -> new HashMap<>();
+			case "kakao" -> (Map<String, Object>) attributes.get("kakao_account");
 			case "google" -> new HashMap<>();
 			default -> throw new IllegalStateException("Unexpected value: " + provider);
 		};
@@ -65,5 +73,10 @@ public class OAuthUserAttributes {
 	private static String getString(Map<String, Object> attributes, String key) {
 		Object value = attributes.get(key);
 		return value != null ? (String) attributes.get(key) : null;
+	}
+
+	private static Long getLong(Map<String, Object> attributes, String key) {
+		Object value = attributes.get(key);
+		return value != null ? (Long) attributes.get(key) : null;
 	}
 }
