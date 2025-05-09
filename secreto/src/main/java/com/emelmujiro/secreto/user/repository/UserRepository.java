1f696f7b1374b080986432ffa610759f082ca894
h-spear <80024307+h-spear@users.noreply.github.com>
2025-05-09 16:13:22 +0900

    Fix: UserRepository import 에러 해결 (#17)

--- secreto/src/main/java/com/emelmujiro/secreto/user/repository/UserRepository.java
+++ secreto/src/main/java/com/emelmujiro/secreto/user/repository/UserRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.emelmujiro.secreto.user.entity.User;
-
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
