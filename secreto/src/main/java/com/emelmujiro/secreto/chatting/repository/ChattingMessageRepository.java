bd43ef15007f7d3a1bb33a683a87f460507b9450
sanghak lee <yee950419@naver.com>
2025-05-11 14:48:11 +0900

    Fix: chatting repository 내 @Param 명 명시
    Related to #15

--- secreto/src/main/java/com/emelmujiro/secreto/chatting/repository/ChattingMessageRepository.java
+++ secreto/src/main/java/com/emelmujiro/secreto/chatting/repository/ChattingMessageRepository.java
@@ -3,14 +3,15 @@ package com.emelmujiro.secreto.chatting.repository;
 import com.emelmujiro.secreto.chatting.entity.ChattingMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChattingMessageRepository extends JpaRepository<ChattingMessage, Long> {
 
     @Query("select cm from ChattingMessage cm where cm.chattingRoom.id = :chattingRoomId")
-    List<ChattingMessage> findAllByChattingRoomId(Long chattingRoomId);
+    List<ChattingMessage> findAllByChattingRoomId(@Param("chattingRoomId") Long chattingRoomId);
 
     @Query("select cm from ChattingMessage cm where cm.chattingRoom.id = :chattingRoomId and cm.id in :chattingMessageIds")
-    List<ChattingMessage> findAllByChattingRoomIdAndChattingMessageId(long chattingRoomId, List<Long> chattingMessageIds);
+    List<ChattingMessage> findAllByChattingRoomIdAndChattingMessageId(@Param("chattingRoomId") Long chattingRoomId, @Param("chattingMessageIds") List<Long> chattingMessageIds);
 }
