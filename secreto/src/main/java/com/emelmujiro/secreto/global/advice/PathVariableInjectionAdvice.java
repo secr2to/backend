214c76bc6b838901f2408fe54ce4fbe38d26a539
h-spear <80024307+h-spear@users.noreply.github.com>
2025-05-17 03:14:18 +0900

    Feat: PathVariable 값이 @RequestBody 객체에 자동 주입되도록 구현
    - PathVariableInjectAdvice 구현
    - InjectPathVariable 애노테이션 생성
    
    Related to #32

---
+++ secreto/src/main/java/com/emelmujiro/secreto/global/advice/PathVariableInjectionAdvice.java
@@ -0,0 +1,115 @@
+package com.emelmujiro.secreto.global.advice;
+
+import static org.springframework.web.servlet.HandlerMapping.*;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+import com.emelmujiro.secreto.global.annotation.InjectPathVariable;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class PathVariableInjectionAdvice implements RequestBodyAdvice {
+
+	@Override
+	public boolean supports(MethodParameter methodParameter, Type targetType,
+		Class<? extends HttpMessageConverter<?>> converterType) {
+		HttpServletRequest request = getHttpServletRequest();
+		Map<String, String> pathVariables = getPathVariables(request);
+		return !pathVariables.isEmpty();
+	}
+
+	@Override
+	public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType,
+		Class<? extends HttpMessageConverter<?>> converterType) {
+
+		HttpServletRequest request = getHttpServletRequest();
+		Map<String, String> pathVariables = getPathVariables(request);
+		injectPathVariableIntoAnnotatedFields(body, pathVariables);
+
+		return body;
+	}
+
+	private void injectPathVariableIntoAnnotatedFields(Object body, Map<String, String> pathVariables) {
+		Arrays.stream(body.getClass().getDeclaredFields())
+			.filter(field -> field.isAnnotationPresent(InjectPathVariable.class))
+			.forEach(field -> {
+				String name = resolveFieldName(field);
+				Class<?> type = field.getType();
+				Object value = getConvertedValue(pathVariables, name, type);
+
+				ReflectionUtils.makeAccessible(field);
+				ReflectionUtils.setField(field, body, value);
+			});
+	}
+
+	private Object getConvertedValue(Map<String, String> pathVariables, String name, Class<?> type) {
+		Object converted;
+		String value = pathVariables.get(name);
+		try {
+			if (Long.class.isAssignableFrom(type) || type == long.class) {
+				converted = Long.parseLong(value);
+			} else if (Integer.class.isAssignableFrom(type) || type == int.class) {
+				converted = Integer.parseInt(value);
+			} else if (Boolean.class.isAssignableFrom(type) || type == boolean.class) {
+				converted = Boolean.parseBoolean(value);
+			} else if (Double.class.isAssignableFrom(type) || type == double.class) {
+				converted = Double.parseDouble(value);
+			} else if (Float.class.isAssignableFrom(type) || type == float.class) {
+				converted = Float.parseFloat(value);
+			} else {
+				converted = pathVariables.get(name);
+			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(
+				String.format("Cannot convert path variable '%s' value '%s' to type %s.",
+					name, value, type)
+			);
+		}
+		return converted;
+	}
+
+	private String resolveFieldName(Field field) {
+		String name = field.getAnnotation(InjectPathVariable.class).name();
+		return (name == null || name.isEmpty()) ? field.getName() : name;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Map<String, String> getPathVariables(HttpServletRequest httpServletRequest) {
+		return (Map<String, String>) httpServletRequest.getAttribute(URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+	}
+
+	private HttpServletRequest getHttpServletRequest() {
+		RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+		if (!(requestAttributes instanceof ServletRequestAttributes)) {
+			throw new IllegalStateException("No current HTTP request available.");
+		}
+		return ((ServletRequestAttributes) requestAttributes).getRequest();
+	}
+
+	@Override
+	public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter,
+		Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return body;
+	}
+
+	@Override
+	public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType,
+		Class<? extends HttpMessageConverter<?>> converterType) throws IOException {
+		return inputMessage;
+	}
+}
