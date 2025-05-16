95b6f171f8592dece0bdd37c1da6f6383651bdd8
h-spear <80024307+h-spear@users.noreply.github.com>
2025-05-17 01:29:17 +0900

    Feat: @RequestBody에서 User ID 필드가 자동으로 주입되도록 구현
    - @InjectUserId 애노테이션 생성
    - UserIdInjectionAdvice 생성
    
    Related to #32

---
+++ secreto/src/main/java/com/emelmujiro/secreto/auth/advice/UserIdInjectionAdvice.java
@@ -0,0 +1,73 @@
+package com.emelmujiro.secreto.auth.advice;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.security.core.Authentication;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+import com.emelmujiro.secreto.auth.annotation.InjectUserId;
+import com.emelmujiro.secreto.auth.dto.SecurityContextUser;
+import com.emelmujiro.secreto.auth.util.SecurityContextUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class UserIdInjectionAdvice implements RequestBodyAdvice {
+
+	@Override
+	public boolean supports(MethodParameter methodParameter, Type targetType,
+		Class<? extends HttpMessageConverter<?>> converterType) {
+		return true;
+	}
+
+	@Override
+	public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType,
+		Class<? extends HttpMessageConverter<?>> converterType) {
+
+		Authentication authentication = SecurityContextUtil.getAuthentication();
+		if (authentication != null && authentication.isAuthenticated()) {
+			SecurityContextUser principal = SecurityContextUtil.getPrincipal();
+			Long userId = principal.userId();
+			injectUserIdIntoAnnotatedFields(body, userId);
+		}
+		return body;
+	}
+
+	private void injectUserIdIntoAnnotatedFields(Object body, Long userId) {
+		Arrays.stream(body.getClass().getDeclaredFields())
+			.filter(this::isInjectUserIdField)
+			.forEach(field -> {
+				ReflectionUtils.makeAccessible(field);
+				ReflectionUtils.setField(field, body, userId);
+			});
+	}
+
+	private boolean isInjectUserIdField(Field field) {
+		boolean hasAnnotation = field.isAnnotationPresent(InjectUserId.class);
+		boolean hasLongType = Long.class.isAssignableFrom(field.getType()) || field.getType() == long.class;
+		return hasAnnotation && hasLongType;
+	}
+
+	@Override
+	public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter,
+		Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return body;
+	}
+
+	@Override
+	public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType,
+		Class<? extends HttpMessageConverter<?>> converterType) throws IOException {
+		return inputMessage;
+	}
+}
