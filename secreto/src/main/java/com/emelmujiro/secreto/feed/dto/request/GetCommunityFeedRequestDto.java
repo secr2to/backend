18232aa372a609dcb8af09764d5735fd42541777
h-spear <80024307+h-spear@users.noreply.github.com>
2025-05-24 00:04:04 +0900

    Refactor: Feed Request DTO 애노테이션 정리

--- secreto/src/main/java/com/emelmujiro/secreto/feed/dto/request/GetCommunityFeedRequestDto.java
+++ secreto/src/main/java/com/emelmujiro/secreto/feed/dto/request/GetCommunityFeedRequestDto.java
@@ -2,12 +2,14 @@ package com.emelmujiro.secreto.feed.dto.request;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Builder
 public class GetCommunityFeedRequestDto {
 
 	private Long feedId;
