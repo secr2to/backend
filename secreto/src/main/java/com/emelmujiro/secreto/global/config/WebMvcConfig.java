cee87439fe67aaa7f82492ab7033c5510ecb886d
sanghak lee <yee950419@naver.com>
2025-07-02 03:49:11 +0900

    Feat: 전체 알림 내용 조회 api 구현
    Related to #60

--- secreto/src/main/java/com/emelmujiro/secreto/global/config/WebMvcConfig.java
+++ secreto/src/main/java/com/emelmujiro/secreto/global/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.emelmujiro.secreto.global.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,8 +18,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(new LoginUserModelAttributeProcessor(true));
-		resolvers.add(new LoginUserModelAttributeProcessor(false));
+		WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+//		resolvers.add(new LoginUserModelAttributeProcessor(true));
+//		resolvers.add(new LoginUserModelAttributeProcessor(false));
 		resolvers.add(new LoginUserArgumentResolver());
 	}
 }
---
+++ secreto/src/main/java/com/emelmujiro/secreto/notification/dto/NotificationDto.java
@@ -0,0 +1,18 @@
+package com.emelmujiro.secreto.notification.dto;
+
+import com.emelmujiro.secreto.notification.entity.NotificationType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class NotificationDto {
+    private String author;
+    private String content;
+    private LocalDateTime generatedDate;
+    private Boolean readYn;
+    private NotificationType type;
+}
