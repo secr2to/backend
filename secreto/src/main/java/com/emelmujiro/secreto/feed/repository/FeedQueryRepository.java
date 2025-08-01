6b8435aa5662e098ee460a4b2a50d604a3bf9d7f
h-spear <80024307+h-spear@users.noreply.github.com>
2025-08-01 21:24:44 +0900

    Feat: 댓글 조회 API 방 유저 프로필 데이터도 가져오도록 수정

--- secreto/src/main/java/com/emelmujiro/secreto/feed/repository/FeedQueryRepository.java
+++ secreto/src/main/java/com/emelmujiro/secreto/feed/repository/FeedQueryRepository.java
@@ -10,17 +10,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
-import com.emelmujiro.secreto.feed.dto.request.GetCommunityRequestDto;
 import com.emelmujiro.secreto.feed.dto.request.GetIngameFeedsRequestDto;
-import com.emelmujiro.secreto.feed.dto.response.CommunityFeedResponseDto;
-import com.emelmujiro.secreto.feed.dto.response.GetCommunityResponseDto;
 import com.emelmujiro.secreto.feed.dto.response.GetIngameFeedsResponseDto;
 import com.emelmujiro.secreto.feed.dto.response.IngameFeedResponseDto;
-import com.emelmujiro.secreto.feed.dto.response.QCommunityFeedResponseDto;
 import com.emelmujiro.secreto.feed.dto.response.QIngameFeedResponseDto;
 import com.emelmujiro.secreto.feed.entity.FeedType;
 import com.emelmujiro.secreto.room.dto.response.QRoomUserProfileResponseDto;
-import com.emelmujiro.secreto.user.dto.response.QUserProfileResponseDto;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.StringExpression;
@@ -38,48 +33,6 @@ public class FeedQueryRepository {
 
 	public FeedQueryRepository(EntityManager em) {
 		this.query = new JPAQueryFactory(em);
-	}
-
-	public GetCommunityResponseDto findCommunityFeeds(GetCommunityRequestDto dto) {
-		List<CommunityFeedResponseDto> content = query
-			.select(new QCommunityFeedResponseDto(
-				feed.id,
-				feed.title,
-				feed.imageCount,
-				feed.heartCount,
-				feed.replyCount,
-				feed.thumbnailImageKey,
-				new QUserProfileResponseDto(
-					feed.author.id,
-					feed.author.username,
-					feed.author.profileUrl
-				),
-				feed.createDate
-			))
-			.from(feed)
-			.leftJoin(feed.author, user)
-			.where(
-				feed.feedType.eq(FeedType.COMMUNITY)
-					.and(feed.deletedYn.eq(false))
-					.and(stringContains(feed.title, dto.getKeyword())
-						.or(stringContains(feed.author.searchId, dto.getKeyword()))
-					)
-			)
-			.orderBy(feed.createDate.desc())
-			.offset(dto.getOffset())
-			.limit(pageSize + 1)
-			.fetch();
-
-		boolean hasNext = false;
-		if (content.size() > pageSize) {
-			content.remove(pageSize);
-			hasNext = true;
-		}
-		return GetCommunityResponseDto.builder()
-			.content(content)
-			.offset(hasNext ? dto.getOffset() + pageSize : -1)
-			.hasNext(hasNext)
-			.build();
 	}
 
 	public GetIngameFeedsResponseDto findIngameFeedsWithoutHeartsAndImages(GetIngameFeedsRequestDto dto) {
